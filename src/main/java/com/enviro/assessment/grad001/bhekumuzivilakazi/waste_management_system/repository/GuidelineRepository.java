149c649cdda37ff4bba7c93a64e74d17150c8d5e
Bhekumuzi Sanele Vilakazi <136991241+SanzaGemini@users.noreply.github.com>
2025-01-17 10:33:33 +0200

    Update GuidelineRepository.java

--- src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/GuidelineRepository.java
+++ src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/GuidelineRepository.java
@@ -2,7 +2,7 @@ package com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.model.Guideline;
+import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.models.Guideline;
 
 public interface GuidelineRepository  extends JpaRepository<Guideline,Long>{
 
