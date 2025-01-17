faf75297b2e76fe7f0476df67cd684d6e4b435a7
Bhekumuzi Sanele Vilakazi <136991241+SanzaGemini@users.noreply.github.com>
2025-01-17 10:32:11 +0200

    Update CategoryRepository.java

--- src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/CategoryRepository.java
+++ src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/CategoryRepository.java
@@ -3,7 +3,7 @@ package com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.model.Category;
+import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.models.Category;
 
 public interface CategoryRepository extends JpaRepository<Category,Long>{
 
