8f478981c061ff1a50362ecbfffd5b8d0ef91394
Bhekumuzi Sanele Vilakazi <136991241+SanzaGemini@users.noreply.github.com>
2025-01-17 10:42:30 +0200

    Update CategoryDTO.java

--- src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/models/CategoryDTO.java
+++ src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/models/CategoryDTO.java
@@ -1,12 +1,18 @@
 package com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 /**
  * Data Transfer Object (DTO) for the Category entity.
  * This class is used to transfer category data between layers (e.g., from controller to service).
  */
 public class CategoryDTO {
 
+    @NotNull(groups = CreateGroup.class, message = "Title cannot be null during Adding")
+    @Size(min = 2, groups = CreateGroup.class, message = "Title must have at least 2 characters")
     private String title;  // The title of the category
+
     /**
      * Gets the title of the category.
      * 
@@ -24,6 +30,4 @@ public class CategoryDTO {
     public void setTitle(String title) {
         this.title = title;
     }
-
-
 }
