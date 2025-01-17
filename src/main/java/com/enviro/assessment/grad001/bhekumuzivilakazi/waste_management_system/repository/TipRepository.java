ac5c2c1f9c04795055c7f5d1613c866c151108f2
Bhekumuzi Sanele Vilakazi <136991241+SanzaGemini@users.noreply.github.com>
2025-01-17 10:33:55 +0200

    Update TipRepository.java

--- src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/TipRepository.java
+++ src/main/java/com/enviro/assessment/grad001/bhekumuzivilakazi/waste_management_system/repository/TipRepository.java
@@ -2,7 +2,7 @@ package com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.model.Tip;
+import com.enviro.assessment.grad001.bhekumuzivilakazi.waste_management_system.models.Tip;
 
 public interface TipRepository extends JpaRepository<Tip,Long> {
 
